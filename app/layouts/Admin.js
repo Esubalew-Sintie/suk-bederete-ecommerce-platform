d907f77c67ab8610270271e4a0a427e3d6f9a35e
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-02-26 22:14:44 +0300

    changes

--- app/layouts/Admin.js
+++ app/layouts/Admin.js
@@ -2,12 +2,10 @@ import React from "react";
 
 // components
 
-
-import AdminNavbar from "../app/components/Navbars/AdminNavbar"
-import Sidebar from "../app/components/Sidebar/Sidebar.js";
-import HeaderStats from "../app/components/Headers/HeaderStats.js";
-import FooterAdmin from "../app/components/Footers/FooterAdmin.js";
-
+import Sidebar from "../components/Sidebar/Sidebar.js";
+import HeaderStats from "../components/Headers/HeaderStats.js";
+import FooterAdmin from "../components/Footers/FooterAdmin.js";
+import AdminNavbar from "../components/Navbars/AdminNavbar.js";
 export default function Admin({ children }) {
   return (
     <>
