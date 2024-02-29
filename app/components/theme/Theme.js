413d51088cdc59e0afd3f1d4e99f1658706cd993
Esubalew_Sintie <esubalewsintie28@gmail.com>
2024-02-29 15:21:02 +0300

    SB-5  theme editing

--- app/components/theme/Theme.js
+++ app/components/theme/Theme.js
@@ -4,17 +4,30 @@ import React from "react";
 
 function Theme({theme}) {
 	return (
-		<div className=" w-80 h-80 text-black pb-6 bg-blue-400  border rounded-md">
-			<Image src={theme.url} alt={theme.url} width={300} height={300} />
+		<div className=" w-80  text-black pb-6 bg-blueGray-200 border rounded-md">
+			<div className=" w-[300px] h-[270px]">
+			<Image
+				src={theme.url}
+				alt={theme.url}
+				width={300}
+				height={270}
+				className="   "
+			/>
+			</div>
 
-			<div className=" bg-red-300 flex justify-between mt-2 mx-1">
+			<div className=" flex justify-between mt-2 mx-1">
 				<div className=" flex flex-col">
-					<Link href="refresh" className=" underline mb-2">
+					<Link
+						href="refresh"
+						className="text-black underline text-sm capitalize hidden lg:inline-block font-semibold"
+					>
 						{theme.themeName}
 					</Link>
-					<p>By Suk Bederete</p>
+					<p className=" mt-3">By Suk Bederete</p>
 				</div>
-				<button className=" bg-green-600 px-4  rounded-md border">Add </button>
+				<button className="bg-blueGray-800 text-white hover:bg-blue-200 active:bg-blueGray-600  active:bg-opacity-100 text-sm font-bold uppercase px-3  rounded shadow hover:shadow-lg outline-none  focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+					Select{" "}
+				</button>
 			</div>
 		</div>
 	);
