32473f4236ccb575d18816ee8baaa78ff2faa430
Berihun <taberihun07@gmail.com>
2024-03-05 04:45:09 +0300

    fix conflict error

--- app/components/WebBuilder/Banner/Banner.js
+++ app/components/WebBuilder/Banner/Banner.js
@@ -5,7 +5,7 @@ function Banner() {
   return (
     <div className=" flex flex-col">
       <div className=" flex justify-between items-center">
-        <h1 className=" mt-4 mb-2  text-2xl font-bold">Banner</h1>{" "}
+        <h1 className=" mt-4 mb-2  text-2xl font-bold">Main Banner</h1>{" "}
         <button className="bg-blueGray-800 text-white hover:bg-blue-200 active:bg-blueGray-600  active:bg-opacity-100 text-sm font-bold uppercase px-3 py-2 rounded shadow hover:shadow-lg outline-none  focus:outline-none mr-1 mb-1 mt-2 ease-linear transition-all duration-150">
           Done
         </button>
