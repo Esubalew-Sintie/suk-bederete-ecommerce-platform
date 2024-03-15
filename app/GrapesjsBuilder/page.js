f69e6a0c15577d86b63fb97551393b03db2194ae
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-03-15 21:15:20 +0300

    grapejs builder trial

---
+++ app/GrapesjsBuilder/page.js
@@ -0,0 +1,11 @@
+import GrapesEditor from "../components/Grapesjs/GrapesjsEditor";
+
+const page = () => {
+  return (
+    <div>
+      <GrapesEditor />
+    </div>
+  );
+};
+
+export default page;
