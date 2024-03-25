aba316313006367418f89e91bc96152b9a6b0d9c
Berihun <taberihun07@gmail.com>
2024-03-26 02:34:12 +0300

    my commit

--- app/components/sitebuilder/pageBuilder/index.js
+++ app/components/sitebuilder/pageBuilder/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Base from '../Base';
 import "../../../../styles/index.css"
 const Pagebuilder = (props) => {
+  console.log(props);
   return (
     <>
       <Base {...props} />
