30ec7336d6ac32911fbeaacae9fcb1801c509e9d
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2024-03-26 22:21:31 +0300

    blocks

---
+++ app/play/page.js
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import "../../styles/page.css";
+import Pagebuilder from "../components/play/pageBuilder";
+function App() {
+  return (
+    <div className="App">
+      {/* <GrapesjsMain/> */}
+      <Pagebuilder />
+    </div>
+  );
+}
+
+export default App;
