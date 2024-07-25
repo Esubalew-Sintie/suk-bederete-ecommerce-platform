3b53b50b54c0354db7fdb2918558950d119fae18
berihun101 <taberihun07@gmail.com>
2024-07-25 04:31:22 +0300

    some changes

--- app/[locale]/[shopId]/page.js
+++ app/[locale]/[shopId]/page.js
@@ -12,10 +12,10 @@ export default function Shop({ params }) {
   const router = useRouter();
   const [cart, setCart] = useState(() => {
     // Initialize cart from local storage if available
-    const savedCart = localStorage.getItem("cart");
-    return savedCart ? JSON.parse(savedCart) : [];
+    const savedCart = [];
+    return savedCart;
   });
-  const [numberOfItems, setNumberOfItems] = useState(cart.length);
+  const [numberOfItems, setNumberOfItems] = useState(0);
 
   useEffect(() => {
     if (data) {
@@ -30,8 +30,10 @@ export default function Shop({ params }) {
       event.preventDefault();
       if (link === "contact") {
         router.push(`/${shopId}/contact`);
+        return;
       } else if (link === "blog") {
         router.push(`/${shopId}/blog`);
+        return;
       } else if (link === "about") {
         router.push(`/${shopId}/about`);
       } else if (link === "shopping-cart") {
@@ -39,60 +41,29 @@ export default function Shop({ params }) {
       }
     };
 
-    const attachEventListeners = () => {
-      const blogLink = document.getElementById("blog");
-      const contactLink = document.getElementById("contact");
-      const aboutLink = document.getElementById("about");
-      const shopCartLink = document.getElementById("shopping-cart");
+    const blogLink = document.getElementById("blog");
+    const contactLink = document.getElementById("contact");
+    const aboutLink = document.getElementById("about");
+    const shopCartLink = document.getElementById("shopping-cart");
+    if (blogLink) {
+      blogLink.addEventListener("click", (event) => handleClick(event, "blog"));
+    }
+    if (contactLink) {
+      contactLink.addEventListener("click", (event) =>
+        handleClick(event, "contact")
+      );
+    }
+    if (aboutLink) {
+      aboutLink.addEventListener("click", (event) =>
+        handleClick(event, "about")
+      );
+    }
+    if (shopCartLink) {
+      shopCartLink.addEventListener("click", (event) =>
+        handleClick(event, "shopping-cart")
+      );
+    }
 
-<<<<<<< HEAD
-      if (blogLink) {
-        blogLink.addEventListener("click", (event) => handleClick(event, "blog"));
-      }
-      if (contactLink) {
-        contactLink.addEventListener("click", (event) => handleClick(event, "contact"));
-      }
-      if (aboutLink) {
-        aboutLink.addEventListener("click", (event) => handleClick(event, "about"));
-      }
-      if (shopCartLink) {
-        shopCartLink.addEventListener("click", (event) => handleClick(event, "shopping-cart"));
-      }
-
-      const addToCartButtons = document.querySelectorAll(".product-cart .add-to-cart-button");
-      addToCartButtons.forEach((button, index) => {
-        button.id = `${index}`;
-        console.log("attached event to ", index);
-        button.addEventListener("click", addToCart);
-      });
-
-      const productDetailButtons = document.querySelectorAll(".product-cart .product-detail-button");
-      productDetailButtons.forEach((button) => {
-        button.addEventListener("click", handleProductDetail);
-      });
-    };
-
-    const detachEventListeners = () => {
-      const blogLink = document.getElementById("blog");
-      if (blogLink) {
-        blogLink.removeEventListener("click", handleClick);
-      }
-
-      const addToCartButtons = document.querySelectorAll(".product-cart .add-to-cart-button");
-      addToCartButtons.forEach((button) => {
-        button.removeEventListener("click", addToCart);
-      });
-
-      const productDetailButtons = document.querySelectorAll(".product-cart .product-detail-button");
-      productDetailButtons.forEach((button) => {
-        button.removeEventListener("click", handleProductDetail);
-      });
-    };
-
-    attachEventListeners();
-    return () => detachEventListeners();
-  }, [router, shopId, homepage?.html]);
-=======
     const addToCartButtons = document.querySelectorAll(
       ".product-cart .add-to-cart-button"
     );
@@ -125,27 +96,29 @@ export default function Shop({ params }) {
     //   });
     // };
   }, [router, shopId, homepage?.html, cart, numberOfItems]);
->>>>>>> main
 
   useEffect(() => {
     const cartItemNumber = document.getElementById("cart-item-number");
     if (cartItemNumber) {
       cartItemNumber.textContent = cart.length;
     }
-  }, [cart]);
+  }, [cart, numberOfItems]);
 
   const handleProductDetail = (event) => {
     event.preventDefault();
+
     const productCart = event.currentTarget.closest(".product-cart");
 
     if (!productCart) {
       console.log("product not found");
       return;
     }
-
     const productId = productCart.id;
+    console.log("product id", productId);
     const productName = productCart.querySelector(".name").textContent.trim();
-    const productPrice = productCart.querySelector(".price-value").textContent.trim();
+    const productPrice = productCart
+      .querySelector(".price-value")
+      .textContent.trim();
     const imgElement = productCart.querySelector("img");
     const url = new URL(imgElement.src);
     const imagePath = url.pathname;
@@ -157,42 +130,64 @@ export default function Shop({ params }) {
       image: imagePath,
       quantity: 1,
     };
-
+    console.log("product added detail item ", productItem);
     localStorage.setItem("productDetailItem", JSON.stringify(productItem));
     router.push(`/${shopId}/product-detail`);
   };
 
   const addToCart = (event) => {
     event.preventDefault();
+
     const productCart = event.currentTarget.closest(".product-cart");
 
     if (!productCart) {
       console.log("product not found");
       return;
     }
-
     const productId = productCart.id;
+    console.log("product id", productId);
     const productName = productCart.querySelector(".name").textContent.trim();
-    const productPrice = productCart.querySelector(".price-value").textContent.trim();
+    const productPrice = productCart
+      .querySelector(".price-value")
+      .textContent.trim();
     const imgElement = productCart.querySelector("img");
     const url = new URL(imgElement.src);
     const imagePath = url.pathname;
 
     const existingProduct = cart.find((item) => item.id === productId);
     if (!existingProduct) {
-      const updatedCart = [
-        ...cart,
-        {
+      if (cart.length === 0) {
+        cart.push({
           id: productId,
           name: productName,
           price: productPrice,
           image: imagePath,
           quantity: 1,
-        },
-      ];
-      setCart(updatedCart);
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      setNumberOfItems(updatedCart.length);
+        });
+        setCart(cart);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        setNumberOfItems(cart.length);
+      } else {
+        const updatedCart = [
+          ...cart,
+          {
+            id: productId,
+            name: productName,
+            price: productPrice,
+            image: imagePath,
+            quantity: 1,
+          },
+        ];
+        setCart(updatedCart);
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
+        setNumberOfItems(cart.length);
+      }
+
+      console.log("this what inside cart", cart);
+      console.log(
+        "Updated cart in local storage:",
+        JSON.parse(localStorage.getItem("cart"))
+      ); // Test local storage
     } else {
       console.log(`Product already in cart: ${productName}`);
     }
@@ -210,12 +205,12 @@ export default function Shop({ params }) {
 
   return (
     <div className="relative">
-      <style>{homepage.css}</style>
-      <div className="fixed rounded-full z-[9999] bg-blueGray-800 top-12 left-0">
+      <div className="fixed rounded-full  z-[9999] bg-blueGray-800 top-12 left-0">
         <MenuBar />
       </div>
       <div dangerouslySetInnerHTML={{ __html: homepage.html }} />
-      
+
+      <style>{homepage.css}</style>
       <script>{homepage.js}</script>
     </div>
   );
